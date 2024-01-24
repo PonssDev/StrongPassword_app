0edb036ebadae7559fa05bd6059870bb476d82ee
Pons <alanponstrabajo@gmail.com>
2024-01-24 17:07:56 +0100

    Landing y register terminado.

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+
+// Nos conectamos a la base de datos
+$conexion = mysqli_connect("localhost","root","","proyecto");
+
+// Almacenamos los datos del formulario en variables
+$nombre = $_POST['nombre'];
+$primerApellido = $_POST['primerApellido'];
+$segundoApellido = $_POST['segundoApellido'];
+$email = $_POST['email'];
+$contrasenya = $_POST['contrasenya'];
+
+
+
+
+
+
+
+
+
+
+
+
